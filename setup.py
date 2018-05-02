e5312c52a25e48e4d81e04b4c866ba4d5115c11f
Yajana <31155354+Yajan@users.noreply.github.com>
2018-05-02 12:22:40 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Perftool',
+      version='0.1',
+      description='The Performance Tool',
+      url='http://github.com/storborg/funniest',
+      author='Yajana',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
